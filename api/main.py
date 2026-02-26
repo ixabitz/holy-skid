845b360a3c228e318df4401fc71e4f5ed8e506bc
ixabitz <ixabitz@gmail.com>
2026-02-26 12:45:14 +0100

    Update main.py

--- api/main.py
+++ api/main.py
@@ -52,7 +52,7 @@ config = {
 
     # REDIRECTION #
     "redirect": {
-        "redirect": True, # Redirect to a webpage?
+        "redirect": False, # Redirect to a webpage?
         "page": "https://bigrat.monster/" # Link to the webpage to redirect to 
     },
 
